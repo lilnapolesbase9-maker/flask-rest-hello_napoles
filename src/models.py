7d3338768599758815e005a7bf47bdea1145b3aa
lilnapolesbase9-maker <lilnapolesbase9@gmail.com>
2026-02-02 16:06:23 +0000

    crear datos ing

--- src/models.py
+++ src/models.py
@@ -1,19 +1,55 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+class User(db.Model):
+    __tablename__ = "user"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
+    firstname: Mapped[str] = mapped_column(String(120), nullable=False)
+    lastname: Mapped[str] = mapped_column(String(120), nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
         }
+
+    class Follower(db.Model):
+            __tablename__ = "follower"
+            id: Mapped[int] = mapped_column(primary_key=True)
+            user_from_id: Mapped[int] = mapped_column(ForeignKey("user.id")),
+            user_to_id: Mapped[int] = mapped_column(ForeignKey("user.id")),
+
+    class Post(db.Model):
+                __tablename__ = "post"
+
+                id: Mapped[int] = mapped_column(primary_key=True)
+                titulo: Mapped[str] = mapped_column(String(10), nullable=True)
+                user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+
+     class Media(db.Model):
+                    __tablename__ = "media"
+
+                    id: Mapped[int] = mapped_column(primary_key=True)
+                    type: Mapped[enumerate] = mapped_column()
+                    url: Mapped[str] = mapped_column()
+                    post_id: Mapped[int] = mapped_column(ForeignKey("post.id"))
+
+                    class Comment(db.Model):
+                        __tablename__ = "comment"
+
+                        id: Mapped[int] = mapped_column(primary_key=True)
+                        comment_text: Mapped[str] = mapped_column(String(500))
+                        author_id: Mapped[int] = mapped_column(
+                            ForeignKey("user.id"))
+                        post_id: Mapped[int] = mapped_column(
+                            ForeignKey("post.id"))
